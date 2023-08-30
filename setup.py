c792762591be4a0c5686be20d7e951c66fe6af33
Balogh Gabor Jozsef <busnes14@gmail.com>
2023-08-30 22:01:19 +0200

    Set version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='g-rpc',
-    version='1.1.3',
+    version='1.1.4',
     description="A simple client and server for rpc methods",
     packages=find_packages(),
     install_requires=[
