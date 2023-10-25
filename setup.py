3d9d37b2157623d8f1f1c81c3035869c157707c1
BGabor01 <busnes14@gmail.com>
2023-10-25 13:12:16 +0200

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='g-rpc',
-    version='1.1.8',
+    version='1.1.9',
     description="A simple client and server for rpc methods",
     packages=find_packages(),
     install_requires=[
